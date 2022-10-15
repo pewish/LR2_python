ba864f049788758af9b3d63f9329cc11121de792
evilapels <105581504+pewish@users.noreply.github.com>
2022-10-15 14:39:32 +0300

    Create Task7_1.py

---
+++ Task7_1.py
@@ -0,0 +1,21 @@
+def cached(func):
+    cache = {}
+ 
+    def a(*args, **kwargs):
+        nonlocal cache
+        if not cache.get(args):
+            g = func(*args, **kwargs)
+            cache[args] = g
+            return g
+        else:
+            return cache[args]
+ 
+    return a
+ 
+ 
+@cached
+def fib(n): 
+    if n == 1 or n == 2: 
+        return 1 
+    else: 
+        return fib(n - 1) + fib(n - 2)
